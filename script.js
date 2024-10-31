860433f8d70cad046a36f616c893fa1713363afd
Maximilian <scientificresearch309@gmail.com>
2024-10-31 01:48:32 +0100

    print the age

--- script.js
+++ script.js
@@ -3,9 +3,16 @@
 const calcAge = birthYear => {
   const age = 2037 - birthYear;
   // console.log(firstName);
+  const printAge = () => {
+    const output = `You are ${age}, born in ${birthYear}`;
+    console.log(output);
+  };
 
+  printAge();
   return age;
 };
 
 const firstName = 'Abbas';
 calcAge(1991);
+// console.log(age); ERROR
+// console.log(printAge()); ERROR
